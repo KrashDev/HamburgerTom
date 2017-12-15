602b2c6c099ec5dea180477ff869f530672ef854
Donny Gabai <mrdgabai@gmail.com>
2017-12-14 23:37:30 -0500

    signature animation

--- app.js
+++ app.js
@@ -9,4 +9,32 @@ $(document).ready(function() {
        $("#clean").animate({left: '300px', opacity: '0.5'}, "slow");
 
     });
+
+    	var first = document.querySelector("#aiJGpFF8V");
+		var firstLengtha = first.getTotalLength();
+        console.log(firstLengtha);
+
+        var second = document.querySelector("#f1G9tOoBLK");
+		var secondLength = second.getTotalLength();
+        console.log(secondLength);
+
+        var third = document.querySelector("#b5gWfnHBtj");
+		var thirdLength = third.getTotalLength();
+        console.log(thirdLength);
+        
+        var fourth = document.querySelector("#b2afMgsppt");
+		var fourthLength = fourth.getTotalLength();
+        console.log(fourthLength);
+        
+        var fifth = document.querySelector("#eInj2OfBP");
+		var fifthLength = fifth.getTotalLength();
+        console.log(fifthLength);
+
+        var six = document.querySelector("#ayFOvH8P4");
+		var sixLength = six.getTotalLength();
+        console.log(sixLength);
+        
+        var seven = document.querySelector("#brmiKq7zu");
+		var sevenLength = seven.getTotalLength();
+		console.log(sevenLength);
 });
